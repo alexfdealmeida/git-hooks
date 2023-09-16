68098c1f21b870fd5e99d9c5b8eb9ab88c61dd1f
alex.almeida <alex.almeida@aliare.co>
2023-09-16 12:42:48 -0300

    Branch: main; Removidas dependências com o Husky. --no-verify-policies-protected-files

--- getting-started.sh
+++ getting-started.sh
@@ -4,14 +4,6 @@ vScriptName="$(basename "$0")"
 vPrefixGettingStarted="[$vScriptName]"
 
 vScriptCopyGitHooks="copy-git-hooks.sh"
-vScriptNodeNpmInstall="node-npm-install.sh"
-
-if [ -f $vScriptNodeNpmInstall ]; then
-	echo "$vPrefixGettingStarted $vScriptNodeNpmInstall"
-	chmod +x $vScriptNodeNpmInstall
-	./$vScriptNodeNpmInstall
-	echo ""
-fi
 
 if [ -f $vScriptCopyGitHooks ]; then
 	echo "$vPrefixGettingStarted $vScriptCopyGitHooks"
